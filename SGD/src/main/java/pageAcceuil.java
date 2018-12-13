139c48be7dbafa94a8fa817eb482f1bc9cc800b8
at560075 <at560075@MI104-03>
2018-12-13 16:58:58 +0100

    Merge Antoine/AbdoulMa
    Conflicts:
    	SGD/src/main/java/Users.java

--- SGD/src/main/java/pageAcceuil.java
+++ SGD/src/main/java/pageAcceuil.java
@@ -15,8 +15,8 @@ import javax.swing.JLabel;
 import javax.swing.JList;
 import javax.swing.ListCellRenderer;
 import javax.swing.SwingUtilities;
-
 /*
+test
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
